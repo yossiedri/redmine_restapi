18553848cd21b58b558a3a697b8e0603a505dfa0
Yossi Edri <4yossiedri@gmail.com>
2012-06-08 07:56:09 +0300

    detach user

--- app/models/issue.rb
+++ app/models/issue.rb
@@ -3,10 +3,9 @@ require 'active_resource'
 
 class Issue < ActiveResource::Base
   
-  self.site = 'http://debug-redmine.mtl.com'
-  self.user = 'yedri'
-  self.password = 'V2363txg'
-  # self.key = '07d0abefe52230132230f6e57650bfaa85189ee7'
+	self.site = RedmineConfig['site']
+	self.user = RedmineConfig['user']
+	self.password = RedmineConfig['password']
 
 end
 
